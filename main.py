1d8732011f30c25743aa636ebad1cebb4f06e379
skyxgods <31897040+k0d31@users.noreply.github.com>
2025-06-14 02:04:59 +0200

    main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from datetime import datetime
 intents = disnake.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# for cogs
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
@@ -16,17 +17,17 @@ for filename in os.listdir("./cogs"):
 async def on_ready():
     print("Bot ready!")
 
-
+# Logging joint on server
 @bot.event
 async def on_member_join(member):
     chnljoin = bot.get_channel(1370428575722897419)
     await chnljoin.send(f"{member.mention} присоеденился на сервер! \nЭто уже {member.guild.member_count} участник.")
 
-
+# Logging exit from server
 @bot.event
 async def on_member_remove(member):
     chnlExit = bot.get_channel(1383224073345171476)
     await chnlExit.send(f"{member.mention} покинул наш сервер! \nЭто был {member.guild.member_count} участник.")
 
-
+#token
 bot.run("token")
